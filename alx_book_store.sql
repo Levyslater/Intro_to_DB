2dbf51e3e49f742864154b97fc13fd4fe2801093
Levyslater <muthui@yandex.com>
2025-07-07 15:55:00 +0300

    create tables and databases if not exists

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,14 +1,14 @@
 /* The database should store information about books, 
 authors, customers, orders, and order details */
 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Authors(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215)
 );
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT PRIMARY KEY NOT NULL,
     title VARCHAR(130),
     price DOUBLE,
@@ -16,18 +16,18 @@ CREATE TABLE Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE Customers(
+CREATE TABLE IF NOT EXISTS Customers(
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
-CREATE TABLE Orders(
+CREATE TABLE IF NOT EXISTS Orders(
     order_id INT PRIMARY KEY,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-CREATE TABLE Order_Details(
+CREATE TABLE IF NOT EXISTS Order_Details(
     orderdetailid INT PRIMARY KEY,
     quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
