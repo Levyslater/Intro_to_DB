ee55ae26d0044452a87ac91e68918de190bee36a
Levyslater <muthui@yandex.com>
2025-07-11 09:46:09 +0300

    populatind a table in a database

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+-- insert data into a table 
+
+USE alx_book_store;
+INSERT INTO Customers VALUES(1, 'Cole Baidoo', 'cbaidoo@sandtech.com',
+                                '123 Happiness Ave.');
---
+++ task_6.sql
@@ -0,0 +1,5 @@
+-- insert multiple rows to a table
+USE alx_book_store;
+INSERT INTO Customers VALUES(2, 'Blessing Malik','bmalik@sandtech.com', '124 Happiness  Ave.');
+INSERT INTO Customers VALUES(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+INSERT INTO Customers VALUES(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
