7a23dc823236cf1d250219618f73b0225e13b612
Levyslater <muthui@yandex.com>
2025-07-11 10:20:29 +0300

    create the table books first

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,10 @@
 -- Describe a table without using the DESCRIBE or EXPLAIN statement
 
+CREATE TABLE IF NOT EXISTS books (
+    book_id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(130),
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
 SHOW CREATE TABLE books;
